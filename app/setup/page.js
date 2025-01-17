0f0ddc42b7e1d4daaa80e0cf4af2bc423d3f14a3
DavidWangwjh <davidwang.wjh@gmail.com>
2025-01-16 16:10:16 -0800

    added sub pages

--- app/setup/page.js
+++ app/setup/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Landing page</h1>
+      <h1>Set up page</h1>
     </div>
   );
 }
--- app/user/page.js
+++ app/user/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Landing page</h1>
+      <h1>User page</h1>
     </div>
   );
 }
